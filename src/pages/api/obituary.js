5f496356926c4d7d9fa750638794687918d2f6fc
G*ra[v]yB0@t^ <cgv15a@acu.edu>
2023-03-06 02:18:53 -0600

    url updated to point to firebase

--- src/pages/api/obituary.js
+++ src/pages/api/obituary.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default async (req, res) => {
   try {
     const response = await axios.post(
-      "http://localhost:2000/find-complexity",
+      "https://us-central1-obituary-gpt.cloudfunctions.net/app/find-complexity",
       req.body
     );
     res.status(200).json(response.data);
